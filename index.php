9dc664f4987977e89abce87309cd386c6d85c196
unknown <sistemas@LSA01-16.salas.melendez>
2018-11-14 19:35:37 -0500

    Actualizacion de archivos

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pruebadeheroku</title>
+</head>
+<body>
+
+<header>
+<h1> esto es una prueba de heroku </h1>
+</header>
+
+<section>
+	<div>este es un mensaje prueba de heroku.</div>
+</section>
+<footer>
+	<ul>
+		<li>Direccion</li>
+		<li>Telefono</li>
+		<li>Email</li>
+		<li>1927</li>
+	</ul>
+</footer>
+
+</body>
+</html>
